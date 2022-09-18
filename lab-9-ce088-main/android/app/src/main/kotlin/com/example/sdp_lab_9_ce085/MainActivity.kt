1ee2f9a584993e3c8decb96cfb969c5dd33e545b
devpatel13 <87147446+devpatel13@users.noreply.github.com>
2022-09-18 20:48:57 +0530

    Add files via upload

---
+++ lab-9-ce088-main/android/app/src/main/kotlin/com/example/sdp_lab_9_ce085/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sdp_lab_9_ce085
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lab-9-ce088-main/lib/main.dart
@@ -0,0 +1,302 @@
+/*import 'package:flutter/material.dart';
+//import 'package:lab8_2/quote_card.dart';
+import 'quote_card.dart'; // due to same directory file no need path
+
+void main() => runApp(MaterialApp(
+  home: EchoList(),
+));
+class EchoList extends StatefulWidget {
+  const EchoList({Key? key}) : super(key: key);
+  @override
+  State<EchoList> createState() => _EchoListState();
+}
+class _EchoListState extends State<EchoList> {
+/*
+List<String> quotes = [
+'The truth is realy pure and never simple',
+'I see humans but no humanity',
+'The time is always right to do what is right'
+];
+// List<String> author = [];
+*/
+/* this will create error..because list is now not of string....
+List<String> quotes = [
+Quote(text: 'The truth is realy pure and never simple',author:
+'jignesh1'),
+];
+*/
+  List<Quote> quotes = [
+    Quote(text: 'I remember you was conflicted misusing your influence',author:
+    'Kendrick lamar'),
+
+    Quote(author: 'Jermaine cole', text: 'If you are broke and clowning a millionaire '
+        'than the joke is on you'),
+    Quote(text: 'before you judge someone walk a mile in his shoes',author:
+    'Marshall mathers'),
+  ];
+  Widget quoteTemplate(quote){
+    return Quotecard(quote: quote,);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent[100],
+      appBar: AppBar(
+        title: Text('Quotes'),
+        centerTitle: true,
+        foregroundColor: Colors.blue,
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: Column(
+// mainAxisAlignment: MainAxisAlignment.center,
+// crossAxisAlignment: CrossAxisAlignment.center,
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+      ),
+    );
+  }
+}
+
+class Quotecard extends StatelessWidget {
+  //Quote quote; //in stateless widget variables are not allowed
+  final Quote quote;
+
+  Quotecard({required this.quote});
+  const Quotecard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(30.0, 50.0, 40.0, 25.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 22.0,
+
+                color: Colors.black,
+
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+
+                color: Colors.red,
+
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+*/
+
+/*import 'package:flutter/material.dart';
+import 'package:sdp_lab_9_ce085/quote.dart'; // Absolute path
+//import 'quote.dart'; // due to same directory file relative path will work
+import 'quote_card.dart'; // Relative path
+
+void main() => runApp(MaterialApp(
+  home: EchoList(),
+));
+class EchoList extends StatefulWidget {
+  @override
+  State<EchoList> createState() => _EchoListState();
+}
+class _EchoListState extends State<EchoList> {
+/*
+List<String> quotes = [
+'The truth is realy pure and never simple',
+'I see humans but no humanity',
+'The time is always right to do what is right'
+];
+// List<String> author = [];
+*/
+/* this will create error..because list is now not of string....
+List<String> quotes = [
+Quote(text: 'The truth is realy pure and never simple',author:
+'jignesh1'),
+];
+*/
+  List<Quote> quotes = [
+    Quote(text: 'I remember you was conflicted misusing your influence',author:
+    'Kendrick Lamar'),
+    Quote(author: 'Jermaine Cole', text: 'To appreciate the sun you got to know'
+        'what rain is'),
+    Quote(text: 'Before judging a person, walk a mile in his shoes',author:
+    'Marshall Mathers'),
+  ];
+/*
+Widget quoteTemplate(quote){
+return QuoteCard(quote: quote,);
+}
+*/
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent[100],
+      appBar: AppBar(
+        title: Text('Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.orangeAccent,
+      ),
+      body: Column(
+// mainAxisAlignment: MainAxisAlignment.center,
+// crossAxisAlignment: CrossAxisAlignment.center,
+// children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+        children: quotes.map((quote) => QuoteCard(
+          quote: quote,
+          delete: () {
+            setState(() {
+              quotes.remove(quote);
+            });
+          },
+        )).toList(),
+      ),
+    );
+  }
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+*/
